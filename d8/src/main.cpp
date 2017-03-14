63306553210c04a811798d47829b0c11db892636
Frederick Hornsey <fred@hornsey.us>
2017-03-13 21:07:04 -0500

    Fixed pit detection with northern flow

--- d8/src/main.cpp
+++ d8/src/main.cpp
@@ -88,7 +88,7 @@ void dem_fill(dem_point_t * dem, dem_index_t w, dem_index_t h) {
 
             // Fill it if it's a pit (the minimum of it and it's neighbors)
             if (current < min) {
-                printf("Fill @ <%lu, %lu>\n", x, y);
+                printf("Fill @ <%lu, %lu>, %f to %f\n", x, y, current, min);
                 dem[current_i] = min;
             }
         }
@@ -98,58 +98,57 @@ void dem_fill(dem_point_t * dem, dem_index_t w, dem_index_t h) {
 void d8(dem_point_t * dem, dem_index_t w, dem_index_t h) {
     dem_index_t size = w * h;
 
+    // First we need to get the flows for each cell
+
     // Flow Direction Matrix (DDIRN) Init
     int * dir = (int *) malloc(sizeof(int) * size);
-
-    //// Square Root Matrix Init
-    //dem_point_t * sqrt = (dem_point_t *) malloc(sizeof(dem_point_t) * size);
-    //if (dir == NULL || sqrt == NULL) {
-    //    fprintf(stderr, "Memory Error in D8!\n");
-    //    exit(EXIT_FAILURE);
-    //}
-
-    //// Fill Squre Root Matrix
-    //for (dem_index_t i = 0; i < size; i++){
-    //    sqrt[i] = dem[i] * M_SQRT1_2;
-    //}
 
     // Need to avoid edge
     dem_index_t _w = w - 1;
     dem_index_t _h = h - 1;
-    dem_point_t point, min, cur;
-    dem_index_t i, min_i;
+    dem_point_t point, max, cur;
+    dem_index_t i, max_i;
+
+    int level_neigbors;
+
+    dem_point_t drops[8];
     for (dem_index_t x = 1; x < _w; x++){
         for (dem_index_t y = 1; y < _h; y++){
             i = w * y + x;
             point = dem[i];
 
-            // Find min
-            min = point;
-            min_i = 8;
+            // Get drops
             for (int v = 0; v < 4; v++) {
                 cur = dem[w * (y + D8_ITER_ROW[v]) + (x + D8_ITER_COL[v])];
-                if (cur < min) {
-                    min = cur;
-                    min_i = v;
-                }
+                drops[v] = point - cur;
             }
             for (int v = 4; v < 8; v++) {
-                //cur = sqrt[w * (y + D8_ITER_ROW[v]) + (x + D8_ITER_COL[v])];
                 cur = dem[w * (y + D8_ITER_ROW[v]) + (x + D8_ITER_COL[v])];
-                if (cur < min) {
-                    min = cur;
-                    min_i = v;
+                drops[v] = (point - cur)/M_SQRT2;
+            }
+
+            // Get larget weighted drop in elevation
+            max = drops[0];
+            max_i = 0;
+            for (int v = 1; v < 8; v++) {
+                cur = drops[v];
+                if (cur > max) {
+                    max = cur;
+                    max_i = v;
+                } else if (cur == max) {
+                    max_i = v;
                 }
             }
 
-            if (min_i == 8) {
-                dir[i] = D8_PIT;
-            } else {
-                dir[i] = D8_VALUES[min_i];
+            if (max < 0) { // undifined direction (3a)
+                dir[i] = -1;
+            } else if (max >= 0) { // 
+                dir[i] = D8_VALUES[max_i];
             }
         }
     }
 
+    // Print Flow directions
     for (dem_index_t x = 1; x < _w; x++){
         for (dem_index_t y = 1; y < _h; y++){
             switch (dir[w * y + x]) {
@@ -274,6 +273,7 @@ int main(int argc, char *argv[]) {
     );
 
     // Process the data
+    matrix[10*w + 10] = 200.0;
     dem_fill(matrix, w, h);
     d8(matrix, w, h);
     dem_print(matrix, w, h);
